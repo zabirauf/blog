38a254e7bbcaf648dcf2c418a9ab8e01230f1aa5
zabirauf <zabirauf@gmail.com>
2021-06-06 07:56:01 +0000

    Deploying to main-nextjs-output from  @ b3ae3a93bee3f67e889054052b2d259fd0355b24 🚀

--- components/FullHeightIframe.js
+++ components/FullHeightIframe.js
@@ -1,15 +1,40 @@
 import { useRef, useCallback, useState } from 'react'
 import Spinner from './Spinner'
 
+const TIME_BETWEEN_CHECKS_MS = 1000
+const MAX_TIME_SINCE_IDLE_MS = 10000
+
 export default function FullHeightIframe(props) {
   const { src } = props
 
   const iframeRef = useRef(null)
-  const hasChangedHeight = useRef(false)
   const [hasLoaded, setHasLoaded] = useState(false)
 
   const onLoaded = useCallback(() => {
-    setTimeout(() => {
+    let timeLastUpdated = Date.now()
+    let timeLastChecked = Date.now() - TIME_BETWEEN_CHECKS_MS // As we want the first time to run
+    const iframeWindow = iframeRef.current.contentWindow
+
+    const onRequestAnimationFrame = () => {
+      const time = Date.now()
+      const timeSinceLastChecked = time - timeLastChecked
+      if (timeSinceLastChecked < TIME_BETWEEN_CHECKS_MS) {
+        // We don't want to check on each request frame but wait in between.
+        // If its till under the limit then request callback for another animation frame.
+        iframeWindow.requestAnimationFrame(onRequestAnimationFrame)
+        return
+      }
+
+      timeLastChecked = time
+
+      const timeSinceLastUpdated = time - timeLastUpdated
+      if (timeSinceLastUpdated >= MAX_TIME_SINCE_IDLE_MS) {
+        // If we have not updated height, then things are stable so we
+        // should be done.
+        return
+      }
+
+      // Find the latest height
       const doc = iframeRef.current.contentDocument
       const body = doc.body,
         html = doc.documentElement
@@ -21,25 +46,25 @@ export default function FullHeightIframe(props) {
         html.offsetHeight
       )
 
-      console.log('Height is', height)
-      console.log(
-        'Various heights',
-        body.scrollHeight,
-        body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
-      )
-
-      if (hasChangedHeight.current) {
-        return
+      const newHeight = `${height}px`
+      if (iframeRef.current.style.height !== newHeight) {
+        // Update the height if its not the same as the current one
+        iframeRef.current.style.height = newHeight
+        timeLastUpdated = time
       }
 
-      iframeRef.current.style.height = `${height + 4}px`
-      hasChangedHeight.current = true
-      setHasLoaded(true)
-    }, 1000)
+      // Lets request again so if the height changes we can react to it
+      iframeWindow.requestAnimationFrame(onRequestAnimationFrame)
+    }
+
+    // Show the iframe so that user can start seeing.
+    setHasLoaded(true)
+
+    iframeWindow.requestAnimationFrame(onRequestAnimationFrame)
   }, [])
+
+  // Give time before first so things are rendered
+  const onIframeLoaded = useCallback(() => setTimeout(onLoaded, 1000), [onLoaded])
 
   return (
     <>
@@ -52,7 +77,7 @@ export default function FullHeightIframe(props) {
         title="Notebook"
         className={!hasLoaded ? 'invisible' : 'bg-white'}
         ref={iframeRef}
-        onLoad={onLoaded}
+        onLoad={onIframeLoaded}
         src={src}
         width="100%"
         height="100%"
