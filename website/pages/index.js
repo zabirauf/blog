561f716354b80798a10b60304e829031ee74e14c
Zohaib Rauf <zabirauf@gmail.com>
2021-06-23 19:49:20 -0700

    Fixed making tags optional

--- website/pages/index.js
+++ website/pages/index.js
@@ -58,7 +58,7 @@ export default function Home({ posts }) {
                             </Link>
                           </h2>
                           <div className="flex flex-wrap">
-                            {tags.map((tag) => (
+                            {tags?.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
                           </div>
