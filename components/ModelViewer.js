7b9a9cee486b45d48c7f57a80a3291c9519598b2
zabirauf <zabirauf@gmail.com>
2021-11-01 02:19:42 +0000

    Deploying to main-nextjs-output from  @ 1f188e22eb3d4f067e3b1aeb1afff46572d3bdef 🚀

---
+++ components/ModelViewer.js
@@ -0,0 +1,26 @@
+import dynamic from 'next/dynamic'
+
+const GoogleModelViewImport = dynamic(() => import('./GoogleModelViewImport'), {
+  ssr: false,
+})
+
+export default function ModelViewer(props) {
+  const { alt, src } = props
+  const style = props.style || {}
+
+  return (
+    <>
+      <GoogleModelViewImport />
+      <model-viewer
+        alt={alt}
+        src={src}
+        style={style}
+        ar
+        ar-modes="webxr scene-viewer quick-look"
+        seamless-poster
+        shadow-intensity="1"
+        camera-controls
+      />
+    </>
+  )
+}
