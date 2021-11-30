d6d85fc2d7378a6813dc2969c223ed899fe0739b
zabirauf <zabirauf@gmail.com>
2021-11-30 08:38:58 +0000

    Deploying to main-slider-deploy from  @ ac2cfb30e61f2670ef16580e99ca28f9bc399339 🚀

--- website/components/MDXComponents.js
+++ website/components/MDXComponents.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import CustomLink from './Link'
 import FullHeightIframe from './FullHeightIframe'
+import ModelViewer from './ModelViewer'
 
 const MDXComponents = {
   Image,
   FullHeightIframe,
+  ModelViewer,
   a: CustomLink,
 }
 
