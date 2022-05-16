18a68a4b47094002e25a0cb30461e360e0fcc8a1
Zohaib Rauf <zabirauf@gmail.com>
2022-05-16 05:15:47 +0000

    Updated project page to point to new tool. Created a basic starter page for the tool

---
+++ website/pages/projects/xcsoar-config.js
@@ -0,0 +1,124 @@
+import siteMetadata from '@/data/siteMetadata'
+import { PageSeo } from '@/components/SEO'
+import { Fragment, useState, useCallback } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import * as ReactDOM from 'react-dom'
+
+export default function About() {
+  return (
+    <>
+      <PageSeo
+        title={`About - ${siteMetadata.author}`}
+        description={`About me - ${siteMetadata.author}`}
+        url={`${siteMetadata.siteUrl}/xcsoar-bluefly`}
+      />
+      <div className="divide-y">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            XCSoar configuration generator
+          </h1>
+        </div>
+        <div className="items-start space-y-2 pt-4">
+          <XCSoarConfigGenerator />
+        </div>
+      </div>
+    </>
+  )
+}
+
+function XCSoarConfigGenerator() {
+  const addButton = useCallback(() => {
+    openAddButtonDialog()
+  }, [])
+  return (
+    <>
+      <button
+        type="button"
+        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={addButton}
+      >
+        + Add
+      </button>
+    </>
+  )
+}
+
+function openAddButtonDialog() {
+  const elem = document.createElement('div')
+  document.body.appendChild(elem)
+  const onDialogClose = () => elem.remove()
+  ReactDOM.render(<AddButtonDialog onClose={onDialogClose} />, elem)
+}
+
+function AddButtonDialog(props) {
+  const { onClose } = props
+  const [open, setOpen] = useState(true)
+
+  const closeCallback = useCallback(
+    (opened) => {
+      setOpen(opened)
+      onClose?.()
+    },
+    [onClose]
+  )
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeCallback}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    abc
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                      Payment successful
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet
+                        labore.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Go back to dashboard
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
